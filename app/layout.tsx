469725e7ea8a81c1338744767979291587bb45dd
southwinds97 <southwinds970521@gmail.com>
2024-05-23 17:45:42 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import { pretendard } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className={pretendard.className}>{children}</body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,45 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+     // screens: {
+    //   // @media (max-width: 1200px) { ... }
+    //   md: { max: '1200px' },
+    //   // @media (max-width: 767px) { ... }
+    //   sm: { max: '767px' },
+    // },
+    extend: {
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
+      colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
+        gray: {
+          400: '#999',
+          500: '#666',
+          600: '#292929',
+        },
+        'point-color1': '#ffce32',
+        'point-color2': '#009223',
+      },
+    },
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
+export default config;
